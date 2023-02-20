a932e7f02e1def4998116793dbf11610ff658fdc
Mendes Eduardo <eduardomn2018@gmail.com>
2023-02-20 16:55:55 -0300

    Função original com api local

--- src/components/TasksList/index.jsx
+++ src/components/TasksList/index.jsx
@@ -46,9 +46,6 @@ function TasksList({ tasks, setTasks, setEditId, setTitleTask }) {
               /> :
               <button className='incompleteTasks'></button>
             }
-
-            {/*  Part that doesn't need an API */}
-
           </button>
           <p>
             {task.title}{' '}
@@ -69,84 +66,6 @@ function TasksList({ tasks, setTasks, setEditId, setTitleTask }) {
           </article>
         </li>
       ))}
-      <li key='test01'
-        className={`task ${theme}`}>
-        <button>
-          <button className='incompleteTasks'></button>
-        </button>
-        <p>
-          To study
-        </p>
-        <article className='config'>
-          <button type="button">
-            <img src='/assets/icon-edit.svg' alt="" />
-          </button>
-          <button
-            type="button">
-            <img src='/assets/icon-cross.svg' alt="" />
-          </button>
-        </article>
-      </li>
-      <li key='test01'
-        className={`task ${theme}`}>
-        <button>
-          <img
-            className='check'
-            src='/assets/icon-check.svg'
-            alt='Completed' />
-        </button>
-        <p>
-          Feed the ducks
-        </p>
-        <article className='config'>
-          <button type="button">
-            <img src='/assets/icon-edit.svg' alt="" />
-          </button>
-          <button
-            type="button">
-            <img src='/assets/icon-cross.svg' alt="" />
-          </button>
-        </article>
-      </li>
-      <li key='test01'
-        className={`task ${theme}`}>
-        <button>
-          <button className='incompleteTasks'></button>
-        </button>
-        <p>
-          15 minutes of meditation
-        </p>
-        <article className='config'>
-          <button type="button">
-            <img src='/assets/icon-edit.svg' alt="" />
-          </button>
-          <button
-            type="button">
-            <img src='/assets/icon-cross.svg' alt="" />
-          </button>
-        </article>
-      </li>
-      <li key='test01'
-        className={`task ${theme}`}>
-        <button>
-          <button className='incompleteTasks'></button>
-        </button>
-        <p>
-          Go to the gym at 13:30
-        </p>
-        <article className='config'>
-          <button type="button">
-            <img src='/assets/icon-edit.svg' alt="" />
-          </button>
-          <button
-            type="button">
-            <img src='/assets/icon-cross.svg' alt="" />
-          </button>
-        </article>
-      </li>
-
-      {/* Until here */}
-
     </ul>
   );
 }
